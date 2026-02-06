1c6f43f6cc5ff1e3adffcefc430592d598de34d8
Z <ccao2@andrew.cmu.edu>
2026-02-05 19:32:09 -0500

    child model created, passes child tests

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,2 +1,10 @@
 class Chore < ApplicationRecord
+  belongs_to :child
+  belongs_to :task
+
+  validates :child_id, presence: true
+  validates :task_id, presence: true
+  validates :due_date, presence: true
+
+  scope :due_soon, -> { where('due_date <= ?', Date.today + 3.days).order(:due_date) }
 end
